722e5507578269a6f1f73356a54e47967d78c3cc
crackcomm <crackcomm@gmail.com>
2016-01-19 15:07:44 +0100

    first commit

---
+++ googlebot.go
@@ -0,0 +1,40 @@
+package googlebot
+
+import (
+	"net"
+	"strings"
+	"time"
+
+	"github.com/patrickmn/go-cache"
+)
+
+var cacheIPs *cache.Cache
+
+func init() {
+	cacheIPs = cache.New(time.Second*360, time.Second*30)
+}
+
+// IsGoogleBot - Checks if given IP address is really Google IP address.
+func IsGoogleBot(addr string) (yes bool, err error) {
+	addrs, err := net.LookupAddr(addr)
+	if err != nil || len(addrs) == 0 {
+		return
+	}
+	yes = strings.HasSuffix(addrs[0], ".googlebot.com.")
+	return
+}
+
+// IsGoogleBotWithCache - Checks if given IP address is really Google IP address.
+// Uses in memory cache.
+func IsGoogleBotWithCache(addr string) (yes bool, err error) {
+	if v, ok := cacheIPs.Get(addr); ok {
+		yes, _ = v.(bool)
+		return
+	}
+	yes, err = IsGoogleBot(addr)
+	if err != nil {
+		return
+	}
+	cacheIPs.Set(addr, yes, cache.DefaultExpiration)
+	return
+}
